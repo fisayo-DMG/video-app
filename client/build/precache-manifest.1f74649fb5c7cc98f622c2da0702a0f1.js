93fcc9577d40f9e59431745db2b850f5d4c84248
Fisayo <aikomofisayo@gmail.com>
2020-05-05 23:59:25 +0100

    Add build folder

---
+++ client/build/precache-manifest.1f74649fb5c7cc98f622c2da0702a0f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3aa0d3ce78e93c97a458a9a23082a878",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e723af783b671c023e5",
+    "url": "/static/css/main.7671b54d.chunk.css"
+  },
+  {
+    "revision": "df792239c2eb8ab423ab",
+    "url": "/static/js/2.4ad4b16e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4ad4b16e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e723af783b671c023e5",
+    "url": "/static/js/main.32e23b8a.chunk.js"
+  },
+  {
+    "revision": "15512df3c76a7ac34ef0",
+    "url": "/static/js/runtime-main.76f74c86.js"
+  }
+]);
